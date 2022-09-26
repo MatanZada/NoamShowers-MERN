5631ccc18b94e48bca51a2d7ad798a8c600659bd
matan <you@example.com>
2022-09-27 00:23:06 +0300

    Adding pages, cart, homepage, login, product, register, login, success

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,146 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://cdn.pixabay.com/photo/2014/07/10/17/18/shower-389273_960_720.jpg")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: #f6f6f6;
+  ${mobile({ width: "75%" })}
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: #fff;
+  cursor: pointer;
+`;
+const Error = styled.span`
+  color: red;
+`;
+
+const Register = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8080/api/users";
+      const { data: res } = await axios.post(url, data);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            onChange={handleChange}
+            value={data.firstName}
+            required
+          />
+
+          <Input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            onChange={handleChange}
+            value={data.lastName}
+            required
+          />
+
+          <Input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+            value={data.email}
+            required
+          />
+
+          <Input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            value={data.password}
+            required
+          />
+
+          {/* <Input placeholder="confirm Password" /> */}
+
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+
+          {error && <Error>Something went wrong...</Error>}
+
+          <Button type="submit">CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
