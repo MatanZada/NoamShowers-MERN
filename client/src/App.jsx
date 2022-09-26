3735c9c3408bb18404c0dc22443b554d47fd8dc9
matan <you@example.com>
2022-09-27 00:23:39 +0300

    Adding redux, routes, data

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+
+      <Route path="products/:category" element={<ProductList />} />
+      <Route path="product/:id" element={<Product />} />
+      <Route
+        path="register"
+        element={user ? <Navigate to="/" /> : <Register />}
+      />
+      <Route path="login" element={user ? <Navigate to="/" /> : <Login />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="success" element={<Success />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,112 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2018/06/Vitra_0256_copy-1.jpg",
+    title: "POPULAR SALE",
+    desc: "Do not compromise on quality! Get a 10% discount on the shower,bathroom cabinet and shower accessories",
+    bg: "EEEEEE",
+  },
+  {
+    id: 2,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2018/06/304_Versus_-_Bathroom_-_White-copy.jpg",
+    title: "POPULAR SALE",
+    desc: "Do not compromise on quality! Get a 10% discount on the shower,bathroom cabinet and shower accessories",
+    bg: "EEEEEE",
+  },
+  {
+    id: 3,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2018/06/Memoria_Elements_-_Set_02_-_01_Main_Set_Revised-1.jpg",
+    title: "POPULAR SALE",
+    desc: "Do not compromise on quality! Get a 10% discount on the shower,bathroom cabinet and shower accessories",
+    bg: "EEEEEE",
+  },
+  {
+    id: 4,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2018/06/439_Bern_-_Bathroom_Set_01_-_02_Ambiente.jpg",
+    title: "POPULAR SALE",
+    desc: "Do not compromise on quality! Get a 10% discount on the shower,bathroom cabinet and shower accessories",
+    bg: "EEEEEE",
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2018/06/eternity_x2-copy.jpg",
+    title: "BATHROOM",
+  },
+  {
+    id: 1,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/07/A42557.jpg",
+    title: "SINK FAUCET",
+  },
+  {
+    id: 1,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2018/07/VitrA6286srevize.jpg",
+    title: "BATHROOM SINK",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/62570_sol.jpg",
+  },
+  {
+    id: 2,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/62582_1-1.jpg",
+  },
+  {
+    id: 3,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/64101.jpg",
+  },
+  {
+    id: 4,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/64079.jpg",
+  },
+  {
+    id: 5,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/07/64110.jpg",
+  },
+  {
+    id: 6,
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/64041.jpg",
+  },
+];
+export const CartProducts = [
+  {
+    id: 1,
+    Product: "Royal shower",
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/62570_sol.jpg",
+    Size: "200*120",
+  },
+  {
+    id: 2,
+    Product: "Royal shower2",
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/62582_1-1.jpg",
+    Size: "200*120",
+  },
+  {
+    id: 3,
+    Product: "Royal shower3",
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/64101.jpg",
+    Size: "200*120",
+  },
+  {
+    id: 4,
+    Product: "Royal shower4",
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/64079.jpg",
+    Size: "200*120",
+  },
+  {
+    id: 5,
+    Product: "Royal shower5",
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/07/64110.jpg",
+    Size: "200*120",
+  },
+  {
+    id: 6,
+    Product: "Royal shower",
+    img: "https://www.el-gal.co.il/wp-content/uploads/2019/06/64041.jpg",
+    Size: "200*120",
+  },
+];
