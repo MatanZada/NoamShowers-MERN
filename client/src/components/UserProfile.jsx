9a4d7428ba73b5eaf9b0b0ac4ef6e431d9a58888
MatanZada <matan.zada1@email.com>
2022-11-14 13:10:09 +0200

    add user profile

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,32 +1,27 @@
-import { Info } from "@material-ui/icons"
-import { Navigate } from "react-router-dom"
-import { useAuth } from "../context/AuthContext"
+import { Info } from "@material-ui/icons";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
+export const withUser = (Component) => {
+  return function WithUser() {
+    const { userData } = useAuth();
 
-
-
-export const withUser = (Component) =>  {
-
-return function WithUser() {
-    const {userData} = useAuth()
-    
-    if(userData) {
-        return <Component user ={userData}/>
+    if (userData) {
+      return <Component user={userData} />;
     }
-    if(userData === undefined)
-        return <div>Loading....</div>
-    return <Navigate to ="/signin"/>    
-}
-}
+    if (userData === undefined) return <div>Loading....</div>;
+    return <Navigate to="/signin" />;
+  };
+};
 
-
-function UserProfile({user}) {
-
-    return <div>
-            <h1>{user.firstName}</h1>
-            <h2>{user.lastName}</h2>
-            <h3>{user.email}</h3>
+function UserProfile({ user }) {
+  return (
+    <div>
+      <h1>{user.firstName}</h1>
+      <h2>{user.lastName}</h2>
+      <h3>{user.email}</h3>
     </div>
+  );
 }
 
-export default withUser(UserProfile)
+export default withUser(UserProfile);
